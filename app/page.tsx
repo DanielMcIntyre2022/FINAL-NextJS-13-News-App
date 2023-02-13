9da9ef19cd1d76842bc495015c423d2353be4f67
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-13 15:55:04 -0500

    created ts type definitions for data fetching response from news API and began writing the fetching function in home page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { categories } from '../constants';
 
-function page() {
+async function HomePage() {
+// fetch the news data 
+const news: NewsResponse = await fetchNews(categories.join(','));
+
   return (
-    <div>Home Page</div>
+    <div>
+      {/* NewsList - news props */}
+    </div>
   )
 }
 
-export default page;
+export default HomePage;
