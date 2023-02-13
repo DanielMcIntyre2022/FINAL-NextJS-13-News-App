23f5083621e689881ee785bc52d772eaaea6928a
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-13 15:58:18 -0500

    created intial framework for fetchNews async fetch function and imported in home page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { categories } from '../constants';
+import fetchNews from '../lib/fetchNews';
 
 async function HomePage() {
 // fetch the news data 
-const news: NewsResponse = await fetchNews(categories.join(','));
+const news: NewsReponse = await fetchNews(categories.join(','));
 
   return (
     <div>
---
+++ lib/fetchNews.ts
@@ -0,0 +1,16 @@
+const fetchNews = async (
+    category?: Category | string,
+    keywords?: string,
+    isDynamic?: boolean
+) => {
+
+    // GraphQL query 
+
+    // fetch function with Next.js caching...
+
+    // Sort function by images vs not images present
+
+    // return response 
+}
+
+export default fetchNews;
